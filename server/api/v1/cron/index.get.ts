f370731ce2f58d74519b0a5408840ab6bcb2f16a
scriptkiddie-5 <hankefey@gmail.com>
2024-04-03 22:34:48 -0400

    cron stuff

--- server/api/v1/cron/index.get.ts
+++ server/api/v1/cron/index.get.ts
@@ -5,21 +5,24 @@ export default defineEventHandler(async (event) => {
     return event.node.res.writeHead(403).end();
 
   const client = await pool.connect();
-
   try {
     await client.query(`
         DELETE FROM sessions 
         WHERE expires_at < NOW()
     `);
 
+    client.release();
+
     return;
   } catch (err) {
     console.log(err);
+
+    client.release();
+
     setResponseStatus(event, 500);
     return {
       message: "An unknown error occurred, try again later",
     };
   } finally {
-    client.release();
   }
 });
