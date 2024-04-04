77f8094855adeb0ce694a00c0edc4019c55d2b54
scriptkiddie-5 <hankefey@gmail.com>
2024-04-03 22:31:46 -0400

    cron job

---
+++ server/api/v1/cron/index.get.ts
@@ -0,0 +1,25 @@
+import pool from "~/server/utils/database";
+
+export default defineEventHandler(async (event) => {
+  if (getHeader(event, "Authorization") !== `Bearer ${process.env.CRON_SECRET}`)
+    return event.node.res.writeHead(403).end();
+
+  const client = await pool.connect();
+
+  try {
+    await client.query(`
+        DELETE FROM sessions 
+        WHERE expires_at < NOW()
+    `);
+
+    return;
+  } catch (err) {
+    console.log(err);
+    setResponseStatus(event, 500);
+    return {
+      message: "An unknown error occurred, try again later",
+    };
+  } finally {
+    client.release();
+  }
+});
