ab6c3515271237abe812ed20ee678d8710850cb4
scriptkiddie-5 <hankefey@gmail.com>
2024-04-27 22:50:35 -0400

    test

--- server/utils/database.ts
+++ server/utils/database.ts
@@ -3,11 +3,11 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
 const pool = new Postgres.Pool({
+  user: "postgres",
   host: "localhost",
   database: "spectrex",
-  port: 5432,
-  user: "postgres",
   password: "gDvQ514UXzUiDwa6",
+  port: 5432,
 });
 const db = drizzle(pool, { schema });
 
