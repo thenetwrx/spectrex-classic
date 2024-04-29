3b178c5b3da522ea75f6b2f51b5fd6f3c1b68f97
scriptkiddie-5 <hankefey@gmail.com>
2024-04-29 09:07:27 -0400

    add back ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "Spectrex",
+      port: "3000",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
+      env: {
+        DISCORD_CLIENT_ID: "1216792581577506826",
+        DISCORD_CLIENT_SECRET: "wk7pkKo_3TPfXS4pB4Tzuxr4j8faqodL",
+        ENCRYPTION_KEY: "0yxXDqvvgXxYCL3PCjQ3WcseFpkCm2Uq",
+        BASE_URL: "https://spectrex.app",
+        DATABASE_HOST: "localhost",
+        DATABASE_PASSWORD: "gDvQ514UXzUiDwa6",
+        CRON_SECRET: "nigga",
+      },
+    },
+  ],
+};
